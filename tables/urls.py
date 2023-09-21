d9ea00f459e91fa78d18c65505de0ed6f413bf83
npaudyal321 <pnabiniw@gmail.com>
2023-09-21 09:22:29 +0545

    related name

--- tables/urls.py
+++ tables/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student
+from .views import student, profile
 
 urlpatterns = [
-    path("student/", student, name="student")
+    path("student/", student, name="student"),
+    path("profile/", profile, name="profile"),
 ]
--- tables/views.py
+++ tables/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student, StudentProfile
 
 
 def student(request):
     students = Student.objects.all()   # [obj1, obj2, obj3]  [{}, {}, {}]
     return render(request, template_name="tables/student.html", context={"infos": students})
+
+
+def profile(request):
+    return render(request, template_name="tables/profile.html",
+                  context={"profiles": StudentProfile.objects.all()})
