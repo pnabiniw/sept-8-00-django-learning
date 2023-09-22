5abe663141c4938180bef517ded2a818e005a52a
npaudyal321 <pnabiniw@gmail.com>
2023-09-22 09:12:50 +0545

    one to many relation

--- tables/admin.py
+++ tables/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Student, StudentProfile
+from .models import Student, StudentProfile, ClassRoom
 
 admin.site.register(Student)
 admin.site.register(StudentProfile)
+admin.site.register(ClassRoom)
--- tables/models.py
+++ tables/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
+class ClassRoom(models.Model):
+    name = models.CharField(max_length=20)
+
+
 # Create your models here.
-class Student(models.Model):
+class Student(models.Model):  # student_set
     name = models.CharField(max_length=20)
     email = models.EmailField()
     age = models.IntegerField()
     address = models.CharField(max_length=20)
     bio = models.TextField()
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE,
+                                  related_name="classroom_students", null=True, blank=True)
 
     def __str__(self):
         return self.name
