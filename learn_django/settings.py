5b3fde9e1c3d398d1bd3183c635b0192ff042932
npaudyal321 <pnabiniw@gmail.com>
2023-09-24 09:32:10 +0545

    many to many relation

--- learn_django/settings.py
+++ learn_django/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'myapp',
     'temp_inheritance',
     'tables',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- tables/models.py
+++ tables/models.py
@@ -23,3 +23,18 @@ class StudentProfile(models.Model):
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
     contact = models.CharField(max_length=14)
     roll_no = models.IntegerField()
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=20)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
