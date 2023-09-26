275ba3bb64d065291314c76a18b61157b27207d7
npaudyal321 <pnabiniw@gmail.com>
2023-09-26 09:27:32 +0545

    forms

--- forms/urls.py
+++ forms/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import student_view
 
 urlpatterns = [
-    path('', student_view)
+    path('', student_view, name="student_view")
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tables.models import Student
 
 
 def student_view(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        bio = request.POST.get('bio')
+        Student.objects.create(name=name, age=age, email=email, address=address, bio=bio)
+        return redirect('student')
     return render(request, template_name='forms/student_view.html')
