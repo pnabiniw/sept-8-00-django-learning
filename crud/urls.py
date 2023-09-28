406411713646bfeb4d201fbab6c8d6fdf5f4f710
npaudyal321 <pnabiniw@gmail.com>
2023-09-28 09:24:00 +0545

    classroom crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import crud_classroom
+from .views import crud_classroom, classroom_delete, classroom_update
 
 
 urlpatterns = [
+    path("classroom/delete/<int:id>/", classroom_delete, name="classroom_delete"),
+    path("classroom/update/<int:id>/", classroom_update, name="classroom_update"),
     path("", crud_classroom, name="crud_classroom")
 ]
--- crud/views.py
+++ crud/views.py
@@ -11,3 +11,20 @@ def crud_classroom(request):
     return render(request, template_name='crud/classroom.html',
                   context={"classrooms": classrooms, "title": "Classroom"})
 
+
+def classroom_delete(request, id):
+    classroom = ClassRoom.objects.get(id=id)
+    if request.method == "POST":
+        classroom.delete()
+        return redirect('crud_classroom')
+    return render(request, template_name="crud/classroom_delete.html", context={"classroom": classroom})
+
+
+def classroom_update(request, id):
+    classroom = ClassRoom.objects.get(id=id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        ClassRoom.objects.filter(id=id).update(name=name)
+        return redirect("crud_classroom")
+    return render(request, template_name="crud/classroom_update.html",
+                  context={"classroom": classroom})
