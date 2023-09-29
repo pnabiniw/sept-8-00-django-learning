c039ab95e5bcab896a604cfe975fc6afa091d166
npaudyal321 <pnabiniw@gmail.com>
2023-09-29 09:31:11 +0545

    student crud

--- crud/admin.py
+++ crud/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, StudentProfile, ClassRoom
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(StudentProfile)
+admin.site.register(ClassRoom)
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import ClassRoom
+from .models import ClassRoom, Student, StudentProfile
 
 
 def crud_classroom(request):
@@ -28,3 +28,21 @@ def classroom_update(request, id):
         return redirect("crud_classroom")
     return render(request, template_name="crud/classroom_update.html",
                   context={"classroom": classroom})
+
+
+def crud_student(request):
+    students = Student.objects.all()
+    return render(request, template_name="crud/student.html", context={"students": students})
+
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        contact = request.POST.get("contact")
+        student = Student.objects.create(name=name, email=email, age=age, classroom_id=2)
+        StudentProfile.objects.create(address=address, contact=contact, student=student)
+        return redirect("crud_student")
+    return render(request, template_name="crud/add_student.html", context={"title": "Add Student"})
