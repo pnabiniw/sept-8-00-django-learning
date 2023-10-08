974aa7c9bacad79bc995b4e69c35e24cff756f35
npaudyal321 <pnabiniw@gmail.com>
2023-10-08 09:31:55 +0545

    get and post student

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import hello_world, HelloWorldView, SimpleStudentView
-
+from .views import hello_world, HelloWorldView, SimpleStudentView, \
+    StudentFromDBView, StudentFromDBListView
 
 urlpatterns = [
     path("hello-world/", hello_world),
     path("message/", HelloWorldView.as_view()),
-    path("simple-student/", SimpleStudentView.as_view())
+    path("simple-student/", SimpleStudentView.as_view()),
+    path("student-from-db/", StudentFromDBListView.as_view()),
+    path("student-from-db/<int:id>/", StudentFromDBView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from crud.models import Student
 
 
 def hello_world(request):
@@ -26,3 +27,47 @@ class SimpleStudentView(APIView):
             {"name": "Alex", "age": 30, "address": "KTM"},
         ]
         return Response(students)
+
+
+class StudentFromDBView(APIView):
+    def get(self, *args, **kwargs):
+        id = kwargs["id"]  # name, email, age, classroom
+        try:
+            student = Student.objects.get(id=id)
+        except Student.DoesNotExist:
+            return Response({
+                "detail": "Invalid Student ID"
+            })
+        response = {
+            "name": student.name,
+            "email": student.email,
+            "age": student.age,
+            "classroom": student.classroom.name
+        }
+        return Response(response)
+
+
+class StudentFromDBListView(APIView):
+    def get(self, *args, **kwargs):
+        """
+                [
+                {"name": "Jon", "age": 30, "email": "a@a.com", "classroom": "One"},
+                {"name": "Jon", "age": 30, "email": "a@a.com", "classroom": "One"},
+                {"name": "Jon", "age": 30, "email": "a@a.com", "classroom": "One"}
+                ]
+        """
+        students = Student.objects.all()
+        response = [{"name": student.name, "age": student.age} for student in students]
+        return Response(response)
+
+    def post(self, *args, **kwargs):
+        # name = request.POST.get("name")
+        print(self.request.data)
+        name = self.request.data.get("name")
+        email = self.request.data.get("email")
+        age = self.request.data.get("age")
+        classroom = self.request.data.get("classroom")
+        Student.objects.create(name=name, email=email, age=age, classroom_id=classroom)
+        return Response({
+            "detail": "Student Created Successfully !!"
+        })
